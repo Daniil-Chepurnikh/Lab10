446a971b98dc4d0e0e7363295a85a4ba91d3a3a7
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-02-18 16:42:32 +0500

    Добавил скелеты свойств и конструкторов

--- Emoji/AnimalEmogi.cs
+++ Emoji/AnimalEmogi.cs
@@ -1,14 +1,53 @@
 ﻿using System;
+using System.Runtime.InteropServices.JavaScript;
 
 namespace LibraryEmoji
 {
     internal class AnimalEmogi : Emoji
     {
-        string bodyPart;
+        string animalPart;
 
         /// <summary>
         /// Часть тела животного в эмодзи
         /// </summary>
-        public string BodyPart { get; set; }
+        public string AnimalPart 
+        { 
+            get => animalPart; 
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Часть тела животного не может быть пустой, состоять только из пробелов или нулевой");
+            }
+        }
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public AnimalEmogi(): base() 
+        {
+            AnimalPart = "Неопределённая часть тела";
+        }
+
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="name">Название эмодзи</param>
+        /// <param name="tag">Тег эмодзи</param>
+        /// <param name="animalPart">Часть тела животного в эмодзи</param>
+        public AnimalEmogi(string name, string tag, string animalPart) : base(name, tag)
+        {
+            AnimalPart = animalPart;
+        }
+
+        /// <summary>
+        /// Конструктор копирования
+        /// </summary>
+        /// <param name="source">Копируемый эмодзи</param>
+        public AnimalEmogi(AnimalEmogi source) : base(source)
+        {            
+            
+            
+            AnimalPart = source.AnimalPart;
+        }
     }
 }
