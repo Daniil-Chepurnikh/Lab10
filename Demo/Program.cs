a5a8a7ee416d14aec92183436258722b1dee21e6
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-02-18 16:42:05 +0500

    Изменил демонстрационную программу

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,16 +2,13 @@
 
 namespace Demo
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Emoji emoji_1 = new Emoji("Рок жив", "Рок вечен");
-            Emoji emoji_2 = new Emoji(emoji_1);
+            Emoji emoji_1 = new("Рок жив", "Рок вечен");
+            Emoji emoji_2 = new(emoji_1);
             emoji_1.Tag = "Рок навсегда";
-
-            Console.WriteLine(emoji_1.Tag);
-            Console.WriteLine(emoji_2.Tag);
         }
     }
 }
