5bb5bba38862d73d28dbe537776739adf94710f1
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-02-18 16:43:20 +0500

    Оставил заготовку свойств Оставил задачи

--- Emoji/Emoji.cs
+++ Emoji/Emoji.cs
@@ -6,21 +6,45 @@
         /// <summary>
         /// Название эмодзи
         /// </summary>
-        public string Name { get; set; }
+        public string Name 
+        {
+            get => name; 
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Название эмодзи не может быть пустым, состоять из пробелов или нулевым");
+
+                // TODO: Добавить проверку на не содержательную строку(пробельные символы и знаки препинания без нормальных слов
+
+                name = value;
+            }
+        }
 
         string tag;
         /// <summary>
         /// Тег эмодзи
         /// </summary>
-        public string Tag { get; private set; }
+        public string Tag 
+        { 
+            get => tag;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Тег эмодзи не может быть пустым, состоять только из пробелов или нулевым");
+
+                // TODO: Добавить проверку на знаки препинания без нормальных слов
+
+                tag = value;
+            }
+        }
 
         /// <summary>
         /// Конструктор без параметров
         /// </summary>
         public Emoji()
         {
-            Name = "No name";
-            Tag = "No name";
+            Name = "Без имени";
+            Tag = "Без тега";
         }
 
         /// <summary>
@@ -28,11 +52,21 @@
         /// </summary>
         /// <param name="emojiNname"></param>
         /// <param name="emojiTag"></param>
-        public Emoji(string emojiNname, string emojiTag)
+        public Emoji(string emojiName, string emojiTag)
         {
-            name = emojiNname;
+            Name = emojiName;
             Tag = emojiTag;
         }
 
+        /// <summary>
+        /// Конструктор копирования
+        /// </summary>
+        /// <param name="source">Копируемый эмодзи</param>
+        public Emoji(Emoji source)
+        {
+            Name = source.Name;
+            Tag = source.Tag;
+        }
+        
     }
 }
