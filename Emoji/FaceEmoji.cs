f79f15470bd1ceb882dc4d84af232d8b85359da7
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-02-18 16:43:52 +0500

    Оставил скелет свойств

--- Emoji/FaceEmoji.cs
+++ Emoji/FaceEmoji.cs
@@ -1,9 +1,46 @@
 ﻿using System;
+using System.Globalization;
+using System.Reflection.PortableExecutable;
 
 namespace LibraryEmoji
 {
     internal class FaceEmoji : Emoji
     {
+        static readonly string[] faces =
+        [
+            ":(", ":)", "^|0_0|^", "(0 + 0(", 
+            "://", ";(", "?:", ":-(", ":-)", "(~` _ ~`(",
+            "- _ -", "'_'"
+        ];
 
+        string face;
+        /// <summary>
+        /// Лицо эмодзи
+        /// </summary>
+        public string Face
+        {
+            get => face;
+            set
+            {
+                
+            }
+        }
+        
+        private ushort strength;
+        /// <summary>
+        /// Сила эмодзи
+        /// </summary>
+        public ushort Strength
+        {
+            get => strength;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfLessThan(value, 0, "Сила лицевой эмодзи не может быть меньше 0");
+
+                ArgumentOutOfRangeException.ThrowIfGreaterThan(value, 10, "Сила лицевой эмодзи не может быть больше 10");
+
+                strength = value;
+            }
+        }
     }
 }
