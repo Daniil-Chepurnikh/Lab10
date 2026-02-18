2e7702e3ce25290e80bfb683a6804a7c45d45b12
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-02-18 15:53:35 +0500

    Начал писать демонстрационную программу

---
+++ Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using LibraryEmoji;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Emoji emoji_1 = new Emoji("Рок жив", "Рок вечен");
+            Emoji emoji_2 = new Emoji(emoji_1);
+            emoji_1.Tag = "Рок навсегда";
+
+            Console.WriteLine(emoji_1.Tag);
+            Console.WriteLine(emoji_2.Tag);
+        }
+    }
+}
