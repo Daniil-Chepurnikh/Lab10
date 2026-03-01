74a533a892ca35dbf5ccddaec4940919e116aac7
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-03-02 00:17:07 +0500

    Смена модификатора видимости

--- Emoji/SmilingEmoji.cs
+++ Emoji/SmilingEmoji.cs
@@ -4,7 +4,7 @@ using System.Linq.Expressions;
 
 namespace LibraryEmoji
 {
-    internal class SmilingEmoji :Emoji
+    public class SmilingEmoji :Emoji
     {
         /// <summary>
         /// возможные причины улыбок для случайного выбора
