9374cfd62f86eab8e9abf87aa502563d64611137
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-03-05 04:37:33 +0500

    Начал заполнять демонстрационную программу

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -6,15 +6,38 @@ namespace Demo
     public class Program
     {
         static void Main(string[] args)
-        {
-            string[] menu =
-            [
+        {           
+            Emoji[] emojis = new Emoji[35];
 
-            ];
-            
-            Emoji[] emojis = new Emoji[20];
+            for (int p = 0; p < emojis.Length; p++)
+            {
+                Random rn = new();
 
-            Input.MenuAction(menu, "Ds,t\n");
+                emojis[p] = rn.Next(4) switch
+                {
+                    0 => new Emoji(rn),
+                    1 => new AnimalEmoji(rn),
+                    2 => new FaceEmoji(rn),
+                    _ => new SmilingEmoji(rn)
+                };
+            }
+
+            for (int p = 0; p < emojis.Length; p++)
+            {
+                Random rn = new();
+
+                switch (rn.Next(2))
+                {
+                    case 0:
+                        Output.Message(emojis[p].Show() + '\n', ConsoleColor.Cyan);
+                        break; 
+                    case 1:
+                        Output.Message(emojis[p].VirtualShow() + '\n', ConsoleColor.Magenta);
+                        break;
+                }
+            }
+
+
 
         }
     }
