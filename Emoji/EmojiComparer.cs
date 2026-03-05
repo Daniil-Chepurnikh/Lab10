94a9864bc2674d4e8e282b1afc0976c451059d69
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-03-05 05:32:56 +0500

    Реализовал по материалам лекций

--- Emoji/EmojiComparer.cs
+++ Emoji/EmojiComparer.cs
@@ -1,12 +1,24 @@
-﻿using System.Collections;
+﻿using LibraryEmoji;
+using System.Collections;
 
 namespace lab_10_v5_ClassLibrary;
 
-// Здесь нужно реализовать свой Comparer
 public class EmojiComparer : IComparer
 {
-    public int Compare(object? x, object? y)
+    public int Compare(object? x, object? y) // какой смог(захотел)
     {
-        throw new NotImplementedException();
+        if (x == null && y == null)
+        {
+            Emoji em1 = (Emoji)x;
+            Emoji em2 = (Emoji)x;
+
+            if (em1.GetHashCode() > em2.GetHashCode())
+                return 1;
+            else if (em1.GetHashCode() < em2.GetHashCode())
+                return -1;
+            else
+                return 0;
+        }
+        throw new ArgumentNullException();
     }
 }
