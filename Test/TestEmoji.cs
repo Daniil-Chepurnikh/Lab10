c84e8cf098b55d21218de6a803fd2bdef0df9dda
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-03-05 20:06:37 +0500

    Начал писать новые тесты после того как посоветовался

--- Test/TestEmoji.cs
+++ Test/TestEmoji.cs
@@ -12,7 +12,23 @@ namespace Tests
 
             Assert.AreEqual("Без названия", e.Name);
             Assert.AreEqual("Без тега", e.Tag);
-        } 
+        }
+
+        [TestMethod]
+        public void TestTagsArray()
+        {
+            string t = Emoji.tags[2];
+
+            Assert.AreEqual("мат", t);
+        }
+
+        [TestMethod]
+        public void TestNamesArray()
+        {
+            string n = Emoji.names[2];
+
+            Assert.AreEqual("мат", n);
+        }
 
     }
 
