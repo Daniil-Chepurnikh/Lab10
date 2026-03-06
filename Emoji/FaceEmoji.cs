fde6e876cec501f543f5851e97efe9cfdbadcf0e
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-03-06 17:39:44 +0500

    Удалил интерфейс IComparable из производного класса

--- Emoji/FaceEmoji.cs
+++ Emoji/FaceEmoji.cs
@@ -6,7 +6,7 @@ namespace LibraryEmoji
     /// <summary>
     /// Класс лицевых эмодзи
     /// </summary>
-    public class FaceEmoji : Emoji, IComparable
+    public class FaceEmoji : Emoji
     {
         static readonly string[] expressions =
         [
@@ -135,6 +135,7 @@ namespace LibraryEmoji
         {
             base.RandomInit();
             Expression = expressions[random.Next(0, expressions.Length)];
+            Strength = (ushort)random.Next(10);
         }
 
         /// <summary>
@@ -143,23 +144,5 @@ namespace LibraryEmoji
         /// <returns>Значение хеш-кода</returns>
         public override int GetHashCode() => base.GetHashCode() + Strength.GetHashCode() +
                                              Expression.GetHashCode();
-
-        /// <summary>
-        /// Как сравнивать лицевые эмодзи
-        /// </summary>
-        /// <param name="obj">Сравниваемый эмодзи</param>
-        /// <returns>Значение согласно спецификации</returns>
-        public int CompareTo(object? obj)
-        {
-            FaceEmoji face = obj as FaceEmoji;
-            ArgumentNullException.ThrowIfNull(face, "Недопустимое значение");
-
-            if (Strength > face.Strength)
-                return 1;
-            else if (Strength < face.Strength)
-                return -1;
- 
-            return 0;
-        }
     }
 }
